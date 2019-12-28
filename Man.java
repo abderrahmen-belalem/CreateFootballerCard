03151e3e7a1ac92fc37580ed96a82937044100a0
Abderrahmen Belalem <55656596+abderrahmen-belalem@users.noreply.github.com>
2019-12-29 00:59:06 +0100

    Create Man.java

---
+++ Man.java
@@ -0,0 +1,56 @@
+public class Man {
+    String firstName;
+    String lastName;
+    String birthdayDate;
+    double height;
+    int weight;
+
+    Man(String firstName, String lastName, String birthdayDate, double height, int weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayDate = birthdayDate;
+        this.height = height;
+        this.weight = weight;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(String birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
