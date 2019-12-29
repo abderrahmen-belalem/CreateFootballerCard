b6079da6c8f932a51c5f88936e4d130067e80771
Abderrahmen Belalem <55656596+abderrahmen-belalem@users.noreply.github.com>
2019-12-29 01:03:02 +0100

    Create PlayerCard.java

---
+++ PlayerCard.java
@@ -0,0 +1,9 @@
+public class PlayerCard {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to CreatePlayerCard");
+        Footballer footballer = new Footballer();
+        footballer.fillInformation();
+        footballer.displayInformation();
+    }
+}
