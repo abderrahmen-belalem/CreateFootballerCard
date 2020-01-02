0931c4d7221104a6b6612c9f056915f30341da32
Abderrahmen Belalem <55656596+abderrahmen-belalem@users.noreply.github.com>
2020-01-02 21:42:21 +0100

    added a rating to the player

--- PlayerCard.java
+++ PlayerCard.java
@@ -5,5 +5,34 @@ public class PlayerCard {
         Footballer footballer = new Footballer();
         footballer.fillInformation();
         footballer.displayInformation();
+        int ratingTemp= (footballer.pace+footballer.physical+footballer.passing+footballer.defending+footballer.shooting+
+                footballer.dribbling)/6;
+        int sum=0;
+        int rating=0;
+        if(footballer.pace>=ratingTemp){
+            sum+=footballer.pace;
+            rating++;
+        }
+        if(footballer.passing>=ratingTemp){
+            sum+=footballer.passing;
+            rating++;
+        }
+        if(footballer.dribbling>=ratingTemp){
+            sum+=footballer.dribbling;
+            rating++;
+        }
+        if(footballer.shooting>=ratingTemp){
+            sum+=footballer.shooting;
+            rating++;
+        }
+        if(footballer.physical>=ratingTemp){
+            sum+=footballer.physical;
+            rating++;
+        }
+        if(footballer.defending>=ratingTemp){
+            sum+=footballer.defending;
+            rating++;
+        }
+        System.out.println("Your rating is : "+sum/rating);
     }
 }
